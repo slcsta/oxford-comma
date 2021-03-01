d3fce7096edbfaaf7722c815db913df48148c12d
Sarah <sarahlchavez@gmail.com>
2021-02-28 21:36:54 -0800

    commit tree 9e90ebfe3ceb4e6dfa11227c22d2e56981f648ed

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
+    # use conditional
+    if array.length == 1
+        array.join
 
+    elsif array.length == 2
+        array.join(" and ")
+
+    elsif array.length >= 3
+        new_string = array[0..-2].join(", ") << ", and " << array[-1]
+    end
 end
